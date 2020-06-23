b74b0c0d870116fd9da6057cc9b5953a8ced54e8
zzfima <zzfima@gmail.com>
2020-06-23 18:42:09 +0300

    start work

---
+++ RSS_Shell/RSS_Module/RSSApplicationModule.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+
+namespace RSS_Module
+{
+    class RSSApplicationModule : IModule
+    {
+        private IRegionManager _regionManager;
+
+        public RSSApplicationModule(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            _regionManager.RegisterViewWithRegion("MainRegion", typeof(View.RSSReaderView));
+        }
+    }
+}
---
+++ RSS_Shell/RSS_Shell/Bootstrapper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.UnityExtensions;
+using System.Windows;
+
+namespace RSS_Shell
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return new Shell();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+            App.Current.MainWindow = (Window)this.Shell;
+            App.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+
+            ModuleCatalog moduleCatalog = (ModuleCatalog)this.ModuleCatalog;
+            moduleCatalog.AddModule(typeof(RSS_Module.View.RSSReaderView));
+        }
+    }
+}
