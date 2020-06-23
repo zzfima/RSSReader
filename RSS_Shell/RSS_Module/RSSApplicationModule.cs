c87ac5b4a480b57a1653273ace4d12906d973303
zzfima <zzfima@gmail.com>
2020-06-23 19:05:43 +0300

    some rfct

--- RSS_Shell/RSS_Module/RSSApplicationModule.cs
+++ RSS_Shell/RSS_Module/RSSApplicationModule.cs
@@ -3,19 +3,31 @@ using Microsoft.Practices.Prism.Regions;
 
 namespace RSS_Module
 {
+    /// <summary>
+    /// Module with attached regions
+    /// </summary>
     public class RSSApplicationModule : IModule
     {
+        #region Fields
         private IRegionManager _regionManager;
+        #endregion
 
+        #region Ctor
         public RSSApplicationModule(IRegionManager regionManager)
         {
             _regionManager = regionManager;
         }
+        #endregion
 
+        #region Methods
+        /// <summary>
+        /// Initialize of RSSApplicationModule by adding regions
+        /// </summary>
         public void Initialize()
         {
             _regionManager.RegisterViewWithRegion("MainRegion", typeof(View.RSSReaderMainView));
             _regionManager.RegisterViewWithRegion("TreeRegion", typeof(View.RssReaderTree));
-        }
+        } 
+        #endregion
     }
 }
--- RSS_Shell/RSS_Module/RelayCommand.cs
+++ RSS_Shell/RSS_Module/RelayCommand.cs
@@ -3,17 +3,30 @@ using System.Windows.Input;
 
 namespace RSS_Module
 {
+    /// <summary>
+    /// Implementation of ICommand 
+    /// </summary>
     public class RelayCommand : ICommand
     {
+        #region Fields
         Action<object> _execteMethod;
         Func<object, bool> _canexecuteMethod;
+        #endregion
 
+        #region Ctor
         public RelayCommand(Action<object> execteMethod, Func<object, bool> canexecuteMethod)
         {
             _execteMethod = execteMethod;
             _canexecuteMethod = canexecuteMethod;
         }
+        #endregion
 
+        #region Methods
+        /// <summary>
+        /// Infrastructure for CanExecute
+        /// </summary>
+        /// <param name="parameter">parameter for an Func</param>
+        /// <returns></returns>
         public bool CanExecute(object parameter)
         {
             if (_canexecuteMethod != null)
@@ -32,9 +45,14 @@ namespace RSS_Module
             remove { CommandManager.RequerySuggested -= value; }
         }
 
+        /// <summary>
+        /// Infrastructure for Execute Action
+        /// </summary>
+        /// <param name="parameter">parameter for an Action</param>
         public void Execute(object parameter)
         {
             _execteMethod(parameter);
-        }
+        } 
+        #endregion
     }
 }
