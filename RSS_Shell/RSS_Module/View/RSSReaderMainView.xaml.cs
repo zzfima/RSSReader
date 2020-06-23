bf80708e5fa6cb34c84ef6514afe42b9fd8ab80f
zzfima <zzfima@gmail.com>
2020-06-23 19:16:41 +0300

    add view

--- RSS_Shell/RSS_Module/View/RSSReaderMainView.xaml.cs
+++ RSS_Shell/RSS_Module/View/RSSReaderMainView.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RSS_Module.Services;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RSS_Module.View
 {
@@ -23,6 +12,21 @@ namespace RSS_Module.View
         public RSSReaderMainView()
         {
             InitializeComponent();
+            BindLatestFeeds();
+        }
+
+        private void BindLatestFeeds()
+        {
+            string url = "http://feeds.feedburner.com/ScottHanselman";
+            var rssService = new RssService(url);
+            icFeeds.ItemsSource = rssService.GetLatest();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string url = txtUrl.Text;
+            var rssService = new RssService(url);
+            icFeeds.ItemsSource = rssService.GetLatest();
         }
     }
 }
