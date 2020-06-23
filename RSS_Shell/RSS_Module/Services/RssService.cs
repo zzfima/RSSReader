a8f5aa5d1b99bb4249a215bc847e79bf4270b070
zzfima <zzfima@gmail.com>
2020-06-23 19:11:46 +0300

    add rss service class

---
+++ RSS_Shell/RSS_Module/Services/RssService.cs
@@ -0,0 +1,33 @@
+﻿using RSS_Module.Model;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace RSS_Module.Services
+{
+    /// <summary>
+    /// Loading RSS feeds from supplied URL
+    /// </summary>
+    public class RssService
+    {
+        public string FeedUrl { get; private set; }
+
+        public RssService(string feedUrl)
+        {
+            FeedUrl = feedUrl;
+        }
+
+        public List<Feed> GetLatest()
+        {
+            var rssFeed = XDocument.Load(FeedUrl);
+
+            var feeds = (from item in rssFeed.Descendants("item")
+                         select new Feed()
+                         {
+                             Title = item.Element("title").Value,
+                             Description = item.Element("description").Value
+                         }).Take(10).ToList<Feed>();
+            return feeds;
+        }
+    }
+}
